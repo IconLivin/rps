497c5326ff876c8d5c014412d528629a29532142
summer school <temp2019@ccam.cmc.unn.net>
2019-07-10 15:43:26 +0300

    coding smthg

--- include/classificator.h
+++ include/classificator.h
@@ -8,3 +8,21 @@
 using namespace cv;
 using namespace cv::dnn;
 using namespace std;
+
+class Classificator
+{
+public:
+	vector<string> classesNames;
+	virtual Mat Classify(Mat image) = 0 {}
+};
+
+class HandClassificator :public Classificator {
+	string path_to_model, path_to_config, path_to_labels;
+	int width, height;
+	Scalar mean;
+	bool swap;
+	Net net;
+public:
+	HandClassificator(string ptm, string ptc, string ptl, int nwidth, int nheight, Scalar nmean = (0, 0, 0, 0), bool srb = 0);
+	Mat Classify(Mat image);
+};
--- samples/main.cpp
+++ samples/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include "classificator.h"
+
 
 int main() 
 {
-	std::cout << "Work!" << std::endl;
+	VideoCapture cap("C:/Users/temp2019/Desktop/Новая папка/CV-SUMMER-CAMP/data/topdogs.mp4");
+	Mat frame;
+	while (true) {
+		cap >> frame;
+
+		imshow("Я люблю собак", frame);
+		char ch = (char)waitKey(2);
+		if (ch == 'q')
+			break;
+	}
+	cout << " " << endl;
 	return 0;
 }
