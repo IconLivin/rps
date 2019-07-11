a160f4e332f86c7b88caf96361a706a28412510f
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2019-07-11 21:29:34 +0300

    it works at home,but 10fps

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include "classificator.h"
-#include "detector.h"
+//#include "detector.h"
 
-
+string label[3] = { "paper","rock", "scissors" };
 int main() 
 {
 	//Initialized arguments
-	string name = "C:\\Users\\temp2019\\Desktop\\rps\\rps\\models\\mobilnet_for_rps.pb";
 	VideoCapture cap(0);
 	Mat frame;
 	cap >> frame;
@@ -25,13 +24,14 @@ int main()
 		
 		minMaxLoc(ds.Classify(crop), 0, &confidence, 0, &classIdPoint);
 		cout << "ClassId:" << classIdPoint.x << endl << "Confidence:" << confidence << endl;
+		string str = " class  " + /*to_string(classIdPoint.x);*/label[classIdPoint.x];
 		rectangle(frame, rect, Scalar(0, 0, 255),2);
+		putText(frame,str,Point(10,100), cv::FONT_HERSHEY_COMPLEX,	1.0, cv::Scalar(0, 0, 0), 2);
 		imshow("Sasha", frame);
 		char c = (char)waitKey(2);
 		if (c == 'q')
 			break;
-			
-	}
+		}
 	
 	cout << " " << endl;
 	return 0;
