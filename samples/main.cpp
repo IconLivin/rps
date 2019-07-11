27e1c8dbb58bf6df61da56fc2f83dd25f779be76
diper1998 <33723506+diper1998@users.noreply.github.com>
2019-07-11 17:24:34 +0300

    Waiting for a model

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
 #include "classificator.h"
+#include "detector.h"
 
 
 int main() 
 {
 	//Initialized arguments
 	string name = "C:\\Users\\temp2019\\Desktop\\rps\\rps\\models\\mobilnet_for_rps.pb";
-	Mat cap;
-	cap = imread("rock.jfif");
-	/*string model_path =" ";
-	string config_path = " ";
-	string label_path = " ";*/
-	HandClassificator ds(name);
+	VideoCapture cap(0);
+	Mat frame;
+	cap >> frame;
+	int x = frame.cols / 8;
+	int y = frame.rows / 8;
+	//HandClassificator ds(name);
+	Rect rect(Point(x,y*8),Point(7*x,4*y));
 	//work with video
+	while (true) {
+		DnnDetector det;
+		vector<DetectedObject> res;
+		cap >> frame;
+		Mat crop;
+		crop = frame(rect);
+		imshow("Sasha", crop);
+		res = det.Detect(crop);
+		while (!res.empty()) {
+			string conf = "Confidence: " + std::to_string(res.back().score);
+			Point leftbottom(res.back().Left, res.back().Bottom);
+			Point righttop(res.back().Right, res.back().Top
+			);
+			cout << leftbottom << " " << righttop << endl;
+			rectangle(frame, rect, Scalar(127, 255, 0), 2);
 
-		Point classIdPoint;
-		double confidence;
-		minMaxLoc(ds.Classify(cap), 0, &confidence, 0, &classIdPoint);
-		cout << "ClassID:" << classIdPoint.x << endl << "Confidence:" << confidence << endl;
-		
+			putText(frame, conf, Size(res.back().Right - 10, res.back().Top - 2), FONT_HERSHEY_COMPLEX_SMALL, 1,
+				Scalar(10, 99, 255), 1, 0);
+			res.pop_back();
+		}
+		rectangle(frame, rect, Scalar(0, 0, 255),2);
+
+		char c = (char)waitKey(2);
+		if (c == 'q')
+			break;
+			
+	}
 	
 	cout << " " << endl;
 	return 0;
